04a2c8941ec201998318a48e28d9ec44697e9e40
jonathan-wideman <jonathan.m.wideman@gmail.com>
2024-12-21 09:58:53 -0500

    fix tailwind animate import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import * as tailwindAnimate from "tailwindcss-animate"
 export default {
   darkMode: ['class'],
   content: [
@@ -80,5 +81,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [tailwindAnimate],
 };
